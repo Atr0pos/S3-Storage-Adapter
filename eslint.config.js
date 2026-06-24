917b17290c1ccadab0724a4a3acbf4f6157bdd33
Atr0pos <piersbrady@gmail.com>
2026-06-24 15:59:09 +0100

    Add ESLint Prettier and editor tooling config

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist', '.yarn', '.pnp.*'] },
+  js.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  prettier,
+);
